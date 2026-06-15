76a835005266c77482aea3cd5cf1408bb3769316
putraaxzy <ptraasw17@gmail.com>
2026-06-15 10:01:00 +0700

    vibrator: implement callback completion thread and stop on off()
    adapted from: https://github.com/swaraloka-lab/hardware_millennium/blob/sixteen/aidl/vibrator/Vibrator.cpp
    
    Signed-off-by: putraaxzy <ptraasw17@gmail.com>

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -9,6 +9,7 @@
 #include "vibrator-impl/Vibrator.h"
 
 #include <android-base/logging.h>
+#include <chrono>
 #include <fstream>
 
 namespace aidl {
@@ -28,8 +29,23 @@ ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
     return ndk::ScopedAStatus::ok();
 }
 
+void Vibrator::stopCompletionThread() {
+    {
+        std::lock_guard<std::mutex> lock(mMutex);
+        mStopped = true;
+    }
+    mCv.notify_one();
+    if (mCompletionThread.joinable())
+        mCompletionThread.join();
+}
+
+Vibrator::~Vibrator() {
+    stopCompletionThread();
+}
+
 ndk::ScopedAStatus Vibrator::off() {
     LOG(INFO) << "Vibrator off";
+    stopCompletionThread();
     /* Reset index before triggering another set of haptics */
     write_haptic_node(index_node, 0);
     write_haptic_node(activate_node, 0);
@@ -39,8 +55,25 @@ ndk::ScopedAStatus Vibrator::off() {
 ndk::ScopedAStatus Vibrator::on(int32_t timeoutMs,
                                 const std::shared_ptr<IVibratorCallback>& callback) {
     LOG(INFO) << "Vibrator on for timeoutMs: " << timeoutMs;
+    stopCompletionThread();
     write_haptic_node(duration_node, timeoutMs);
     write_haptic_node(activate_node, 1);
+
+    if (callback != nullptr) {
+        {
+            std::lock_guard<std::mutex> lock(mMutex);
+            mStopped = false;
+        }
+        mCompletionThread = std::thread([this, timeoutMs, callback]() {
+            std::unique_lock<std::mutex> lock(mMutex);
+            if (!mCv.wait_for(lock, std::chrono::milliseconds(timeoutMs),
+                              [this] { return mStopped; })) {
+                lock.unlock();
+                callback->onComplete();
+            }
+        });
+    }
+
     return ndk::ScopedAStatus::ok();
 }
 
@@ -96,7 +129,7 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength strength,
     /* Setup effect index */
     write_haptic_node(index_node, index);
 
-    status = on(timeMs, nullptr);
+    status = on(timeMs, callback);
     if (!status.isOk()) {
         return status;
     } else {
